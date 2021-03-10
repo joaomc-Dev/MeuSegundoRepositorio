4ff5bfcccca83bb9afb62c90f1f04c41f102aae1
joaom <joaolprime@gmail.com>
2021-03-10 14:39:27 -0300

    !adicionei getNome, setNome, calculaMatricula

---
+++ pessoa.java
@@ -0,0 +1,17 @@
+Class pessoa {
+    private String nome;
+    private Int matricula;
+    private String sexo;
+
+
+    public void getNome(nome) {
+        return this.nome;
+    }
+    public void setNome(nome) {
+        this.nome = nome;
+    }
+    public int calcularMatricula(matricula)  { 
+            return matriculas *10
+            
+
+            
